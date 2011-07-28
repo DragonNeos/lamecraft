4ce09707a93122636b450dcb6b80aaef466b1846
marcin.ploska <marcin.ploska@gmail.com>
2011-07-28 20:49:07 +0000

    - additional parameter for logger to log messages. - small error fix in shadows generation
    ! Draan - fix that tree generation :p Waaay to many of them are right now on map...

--- source/Aurora/Utils/Logger.cpp
+++ source/Aurora/Utils/Logger.cpp
@@ -13,27 +13,35 @@ namespace Aurora
 
 		void Logger::LogMessage(const char *message, ...)
 		{
-			va_list argList;
-			char cbuffer[1024];
-			va_start(argList, message);
-			vsnprintf(cbuffer, 1024, message, argList);
-			va_end(argList);
-
-			// Written Bytes
-			int written = 0;
-
-			// Open File for Appending
-			SceUID file = sceIoOpen("Log.txt", PSP_O_APPEND | PSP_O_CREAT | PSP_O_WRONLY, 0777);
-
-			// Opened File
-			if(file >= 0)
+			if(logActive)
 			{
-				// Write Buffer
-				written = sceIoWrite(file, cbuffer, strlen(cbuffer));
+				va_list argList;
+				char cbuffer[1024];
+				va_start(argList, message);
+				vsnprintf(cbuffer, 1024, message, argList);
+				va_end(argList);
 
-				// Close File
-				sceIoClose(file);
+				// Written Bytes
+				int written = 0;
+
+				// Open File for Appending
+				SceUID file = sceIoOpen("Log.txt", PSP_O_APPEND | PSP_O_CREAT | PSP_O_WRONLY, 0777);
+
+				// Opened File
+				if(file >= 0)
+				{
+					// Write Buffer
+					written = sceIoWrite(file, cbuffer, strlen(cbuffer));
+
+					// Close File
+					sceIoClose(file);
+				}
 			}
+		}
+
+		void Logger::SetLoggerActive(bool on)
+		{
+			logActive = on;
 		}
 	}
 }
