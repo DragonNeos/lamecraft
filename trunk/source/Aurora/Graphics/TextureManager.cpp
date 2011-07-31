a29e978a40e09449348053ec833bbe1353b66160
marcin.ploska@gmail.com <marcin.ploska@gmail.com@bcad141b-5f7c-f04a-8fdc-afc46acfd1b8>
2011-07-31 10:46:34 +0000

    - added zip file support for texture loading   credits goes to pge engine :)

--- trunk/source/Aurora/Graphics/TextureManager.cpp
+++ trunk/source/Aurora/Graphics/TextureManager.cpp
@@ -129,6 +129,22 @@ namespace Aurora
 			return true;
 		}
 
+		void TextureManager::LoadTexture(std::string fileName,const unsigned char* data,int len)
+		{
+			//check if texture is loaded
+			if(TextureExist(fileName))
+				return;
+
+			Image *image = LoadPngFromMemory(data,len,GU_PSM_8888,1,0);
+			if (image != NULL)
+			{
+				Names.push_back(fileName);
+				Images.push_back(image);
+				Utils::Logger::Instance()->LogMessage("Loaded texture: %s\n",fileName.c_str());
+			}else
+				Utils::Logger::Instance()->LogMessage("Can't open file %s\n",fileName.c_str());
+		}
+
 
 		void TextureManager::LoadTexture(std::string fileName)
 		{
@@ -176,6 +192,159 @@ namespace Aurora
 		Image* TextureManager::LoadPngFromMemory(std::string fileName)
 		{
 			return NULL;
+		}
+
+		typedef struct {
+			const unsigned char *data;
+			png_size_t size;
+			png_size_t seek;
+		} PngData;
+
+		void ReadPngData(png_structp png_ptr, png_bytep data, png_size_t length)
+		{
+			PngData *pngData = (PngData*) png_get_io_ptr(png_ptr);
+			if (pngData) {
+		        int j;
+				for (j = 0; j < length; j++)
+				{
+					if (pngData->seek >= pngData->size) break;
+					data[j] = pngData->data[pngData->seek++];
+				}
+			}
+		}
+
+		Image* TextureManager::loadPngImageImpl(png_structp png_ptr, int len,int ColorMode,int Swizzle,int Vram)
+		{
+			unsigned short *Buffer;
+			unsigned short *swizzled_pixels = NULL;
+			png_infop info_ptr;
+			unsigned int sig_read = 0;
+			png_uint_32 width, height;
+			int bit_depth, color_type, interlace_type, x, y;
+			unsigned int* line;
+			int OutBytesPerPixel;
+			int Power2Width = 0;
+			int Power2Height = 0;
+
+			if(ColorMode == GU_PSM_4444 || ColorMode == GU_PSM_5650 || ColorMode == GU_PSM_5551)OutBytesPerPixel = 2;
+			else OutBytesPerPixel = 4;
+
+			info_ptr = png_create_info_struct(png_ptr);
+			if (info_ptr == NULL)
+			{
+				png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+				return NULL;
+			}
+
+			png_set_sig_bytes(png_ptr, sig_read);
+			png_read_info(png_ptr, info_ptr);
+			png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, &interlace_type, int_p_NULL, int_p_NULL);
+
+			png_set_strip_16(png_ptr);
+			png_set_packing(png_ptr);
+
+			if (color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
+			if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_expand_gray_1_2_4_to_8(png_ptr);
+			if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
+
+			png_set_filler(png_ptr, 0xff, PNG_FILLER_AFTER);
+			line = (unsigned int*) malloc(width * 4);
+			if (!line)
+			{
+				png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+				return NULL;
+			}
+
+			Power2Width = PowerOfTwo(width);
+			Power2Height = PowerOfTwo(height);
+			Buffer = (unsigned short*)memalign(16,Power2Width*Power2Height*OutBytesPerPixel);
+
+			for (y = 0; y < height; y++)
+			{
+				png_read_row(png_ptr, (unsigned char*) line, NULL);
+				for (x = 0; x < width; x++)
+				{
+					unsigned int *Buffer32 = (unsigned int*)Buffer;
+					unsigned int color32 = line[x];
+					unsigned short color16;
+					if(ColorMode == GU_PSM_5551)
+					{
+						color16 = Color8888To5551(color32);
+						Buffer[y*Power2Width+x] = color16;
+					}
+					else if(ColorMode == GU_PSM_4444)
+					{
+						color16 = Color8888To4444(color32);
+						Buffer[y*Power2Width+x] = color16;
+					}
+					else if(ColorMode == GU_PSM_5650)
+					{
+						color16 = Color8888To5650(color32);
+						Buffer[y*Power2Width+x] = color16;
+					}
+					else
+					{
+						Buffer32[y*Power2Width+x] = color32;
+					}
+				}
+			}
+			free(line);
+			png_read_end(png_ptr, info_ptr);
+			png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+
+			Image *Image1 = new Image();
+
+			Image1->Width = width;
+			Image1->Height = height;
+			Image1->power2Width = Power2Width;
+			Image1->power2Height = Power2Height;
+			Image1->vRam = Vram;
+			Image1->ColorMode = ColorMode;
+			Image1->Swizzle = Swizzle;
+
+			if(Swizzle == 1)
+			{
+				if (Vram == 1)
+				{
+					swizzled_pixels = (unsigned short*)valloc(Image1->power2Height*Image1->power2Width*OutBytesPerPixel);
+
+				}else
+				{
+					swizzled_pixels = (unsigned short*)memalign(16,Image1->power2Height*Image1->power2Width*OutBytesPerPixel);
+				}
+				swizzle_fast((u8*)swizzled_pixels,(const u8*)Buffer,Image1->power2Width*OutBytesPerPixel,Image1->power2Height);
+
+				sceKernelDcacheWritebackAll();
+
+				Image1->ImageData = swizzled_pixels;
+				free(Buffer);
+			}else
+			{
+				Image1->ImageData = Buffer;
+			}
+
+			//sceGuFinish();
+			//sceGuSync(0,0);
+
+			return Image1;
+		}
+
+		Image* TextureManager::LoadPngFromMemory(const unsigned char* data,int len,int ColorMode,int Swizzle,int Vram)
+		{
+			if (len < 8) return NULL;
+
+			png_structp png_ptr;
+			png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+			if (png_ptr == NULL) {
+				return NULL;;
+			}
+			PngData pngData;
+			pngData.data = data;
+			pngData.size = len;
+			pngData.seek = 0;
+			png_set_read_fn(png_ptr, (void *) &pngData, ReadPngData);
+			Image* image = loadPngImageImpl(png_ptr, len,ColorMode,Swizzle,Vram);
+			return image;
 		}
 
 		Image* TextureManager::LoadPng(const char* filename,int ColorMode,int Swizzle,int Vram)
